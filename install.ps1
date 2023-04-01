734cf2b8aad8491defb7bcfcda4096711e7c5132
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-04-01 13:53:16 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,3 +9,23 @@ Script by:      https://github.com/bonben365
 For detailed script execution: https://bonben365.com/
 =================================================================================================================
 #>
+#Install and update Desktop framework packages
+$location = "~/Downloads"
+Set-Location $location
+$uri = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Invoke-WebRequest -Uri $uri -OutFile 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+#Download the package to the Downloads folder of current logged on user
+$url = 'https://github.com/microsoft/terminal/releases/latest'
+$request = [System.Net.WebRequest]::Create($url)
+$response = $request.GetResponse()
+$tagUrl = $response.ResponseUri.OriginalString
+$version = $tagUrl.split('/')[-1].Trim('v')
+$fileName = "Microsoft.WindowsTerminal_Win10_$($version)_8wekyb3d8bbwe.msixbundle"
+$downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
+Invoke-WebRequest -Uri $downloadUrl -OutFile "$($location)/$($fileName)" 
+
+#Install Windows Terminal
+$path = Get-ChildItem -Name "*Microsoft.WindowsTerminal*"
+Add-AppxPackage -Path $path
