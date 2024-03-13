c5e98d5b414893ebb7bc663316357d31e834784c
Claudio Bartoli <claudio@barto.li>
2024-03-13 14:04:20 +0100

    fix script

--- install.ps1
+++ install.ps1
@@ -10,7 +10,7 @@ For detailed script execution: https://bonben365.com/
 =================================================================================================================
 #>
 #Install and update Desktop framework packages
-Set-Location $env:temp
+Push-Location $env:temp
 $uri = 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
 (New-Object Net.WebClient).DownloadFile($uri, "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx")
 Add-AppxPackage -Path 'Microsoft.VCLibs.x64.14.00.Desktop.appx'
@@ -23,10 +23,10 @@ $request = [System.Net.WebRequest]::Create($url)
 $response = $request.GetResponse()
 $tagUrl = $response.ResponseUri.OriginalString
 $version = $tagUrl.split('/')[-1].Trim('v')
-$fileName = "Microsoft.WindowsTerminal_Win10_$($version)_8wekyb3d8bbwe.msixbundle"
+$fileName = "Microsoft.WindowsTerminal_$($version)_8wekyb3d8bbwe.msixbundle"
 $downloadUrl = $tagUrl.Replace('tag', 'download') + '/' + $fileName
-(New-Object Net.WebClient).DownloadFile($downloadUrl, "$($env:temp)/$($fileName)")
+(New-Object Net.WebClient).DownloadFile($downloadUrl, "$env:temp\$fileName")
 
 #Install Windows Terminal
-$path = Get-ChildItem -Name "*Microsoft.WindowsTerminal*"
-Add-AppxPackage -Path $path
+Add-AppxPackage -Path "$env:temp\$fileName"
+Pop-Location
